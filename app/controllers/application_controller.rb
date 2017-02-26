b398334a300c2740c6d5df0195ede6e250c59aa0
Sho Kusano <rosylilly@aduca.org>
2017-02-26 13:54:41 +0900

    Force domain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,17 @@
 class ApplicationController < ActionController::Base
   include Sessionable
 
+  before_action :force_domain if Rails.env.production?
+
   protect_from_forgery with: :exception
 
   private
+
+  def force_domain
+    return if ENV['FORCE_DOMAIN'].blank?
+
+    redirect_to("https://#{ENV['FORCE_DOMAIN']}/", status: 301) if request.host != ENV['FORCE_DOMAIN']
+  end
 
   def peek_enabled?
     Rails.env.development?
