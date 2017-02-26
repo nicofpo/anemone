e2e223079b599a1ee4c62a29db1425917850efd8
Sho Kusano <rosylilly@aduca.org>
2017-02-26 13:44:13 +0900

    Setup secret protection

--- app/controllers/api/videos_controller.rb
+++ app/controllers/api/videos_controller.rb
@@ -1,5 +1,9 @@
 class Api::VideosController < Api::ApplicationController
   def create
+    unless Rack::Utils.secure_compare(params[:secret].to_s, Rails.application.secrets.rudbeckia_secret.to_s)
+      return head :forbidden
+    end
+
     ApplicationRecord.transaction do
       params[:videos].each do |video|
         Nico::Video.register(video)
